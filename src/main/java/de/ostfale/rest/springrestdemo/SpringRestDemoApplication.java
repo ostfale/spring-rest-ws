424083104eb03224ae6fd520225f75032ce124f5
Uwe Sauerbrei <info@uwe-sauerbrei.de>
2018-03-14 20:40:41 +0100

    Initial commit

---
+++ src/main/java/de/ostfale/rest/springrestdemo/SpringRestDemoApplication.java
@@ -0,0 +1,12 @@
+package de.ostfale.rest.springrestdemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringRestDemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringRestDemoApplication.class, args);
+	}
+}
