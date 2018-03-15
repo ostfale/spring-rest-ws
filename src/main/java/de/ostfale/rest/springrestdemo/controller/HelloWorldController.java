3ec01154ad83878d7ec7932586bf231cb80a1786
Uwe Sauerbrei <info@uwe-sauerbrei.de>
2018-03-15 22:12:40 +0100

    Path Variable
    + added REST function to display a entered variable

--- src/main/java/de/ostfale/rest/springrestdemo/controller/HelloWorldController.java
+++ src/main/java/de/ostfale/rest/springrestdemo/controller/HelloWorldController.java
@@ -2,6 +2,7 @@ package de.ostfale.rest.springrestdemo.controller;
 
 import de.ostfale.rest.springrestdemo.bean.HelloWorldBean;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 /**
@@ -21,5 +22,10 @@ public class HelloWorldController {
     @GetMapping(path = "/hello-world-bean")
     public HelloWorldBean helloWorldBean() {
         return new HelloWorldBean("Hello World from Bean");
+    }
+
+    @GetMapping(path = "/hello-world-bean/path-variable/{name}")
+    public HelloWorldBean helloWorldPathVariable(@PathVariable String name) {
+        return new HelloWorldBean(String.format("Hello World, %s", name));
     }
 }
